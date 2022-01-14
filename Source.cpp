b546902e8fa6e363db0c5e7c17f530afe6ee187e
bomgara228 <92594974+bomgara228@users.noreply.github.com>
2022-01-14 19:36:32 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include "card.h"
+#include "hand.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Card c1(Card::TWO, Card::CLUBS);
+	c1.printCard();
+	Hand h1;
+	h1.addCard(c1);
+	h1.printHand();
+	h1.getTotal();
+	cout << h1.getScore() << endl;
+	return 0;
+}
